965edb87943976206c55110a60185c246fa9e6d0
Vanditi <kr.pooja.rai@gmail.com>
2017-08-28 11:56:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function(req,res){
+    res.send('Article one requestes')
+})
+app.get('/article-two',function(req,res){
+    res.send('Article one requestes')
+})
+app.get('/article-two',function(req,res){
+    res.send('Article one requestes')
+})
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
